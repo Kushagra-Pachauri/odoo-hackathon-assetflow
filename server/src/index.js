9377469a9318b069b5fb950f6d04175cead0acb8
Shuvam Bhatt <shuvambhattsb2021@gmail.com>
2026-07-12 09:49:44 +0530

    feat: initialize database connection and synchronize server dependencies

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
